3d3e99959cc1cca523bd232276a5f5cdd79436a1
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2021-01-16 13:04:07 -0600

    Implement repeat_xor()

---
+++ set1/challenge5/repeat_xor.py
@@ -0,0 +1,24 @@
+import sys
+
+from set1.challenge2.fixed_xor import xor
+
+
+def repeat_xor(b: bytes, key: bytes) -> bytes:
+    """
+    params:
+        b: bytes to be XORed
+        key: key used for encryption
+    returns:
+        result of XORing bytes of `b` with bytes of `key` by cycling through
+        the `key`
+    """
+    if not key:
+        return b
+
+    operand = b''
+    j = 0
+    key_len = len(key)
+    for i in range(len(b)):
+        operand += key[j].to_bytes(1, byteorder=sys.byteorder)
+        j = (j+1)%key_len
+    return xor(b, operand)
