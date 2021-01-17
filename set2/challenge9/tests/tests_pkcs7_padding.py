d3a1c6ff62faaff93d65ccd1e8aa700e6ade61c1
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2021-01-17 11:33:33 -0600

    Implement pkcs7_pad()

---
+++ set2/challenge9/tests/tests_pkcs7_padding.py
@@ -0,0 +1,58 @@
+import unittest
+
+from set2.challenge9.pkcs7_padding import *
+
+
+class TestPKCS7Padding(unittest.TestCase):
+
+    def test_pkcs7_pad_none_bytes_raises_typeerror(self):
+        with self.assertRaises(TypeError):
+            pkcs7_pad(None, 1)
+
+    def test_pkcs7_pad_empty_bytes_adds_padding(self):
+        b = b''
+        expected_padded = b"\x04\x04\x04\x04"
+
+        actual_padded = pkcs7_pad(b, 4)
+
+        self.assertEqual(expected_padded, actual_padded)
+
+    def test_pkcs7_pad_zero_blk_size_returns_bytes_unchanged(self):
+        b = b'YELLOW SUBMARINE'
+        expected_padded = b"YELLOW SUBMARINE"
+
+        actual_padded = pkcs7_pad(b, 0)
+
+        self.assertEqual(expected_padded, actual_padded)
+
+    def test_pkcs7_pad_negative_blk_size_returns_bytes_unchanged(self):
+        b = b'YELLOW SUBMARINE'
+        expected_padded = b"YELLOW SUBMARINE"
+
+        actual_padded = pkcs7_pad(b, -1)
+
+        self.assertEqual(expected_padded, actual_padded)
+
+    def test_pkcs7_pad_blk_size_over_bounds_returns_bytes_unchanged(self):
+        b = b'YELLOW SUBMARINE'
+        expected_padded = b"YELLOW SUBMARINE"
+
+        actual_padded = pkcs7_pad(b, 256)
+
+        self.assertEqual(expected_padded, actual_padded)
+
+    def test_pkcs7_pad_cryptopals_case(self):
+        b = b"YELLOW SUBMARINE"
+        expected_padded = b"YELLOW SUBMARINE\x04\x04\x04\x04"
+
+        actual_padded = pkcs7_pad(b, 20)
+
+        self.assertEqual(expected_padded, actual_padded)
+
+    def test_pkcs7_pad_bytes_size_mutliple_of_blk_size_adds_extra_block(self):
+        b = b"YELLOW"
+        expected_padded = b"YELLOW\x03\x03\x03"
+
+        actual_padded = pkcs7_pad(b, 3)
+
+        self.assertEqual(expected_padded, actual_padded)
