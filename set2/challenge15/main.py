d8fa6a49cbc141f0c93dabb664288603d5e8554c
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2021-01-22 19:08:40 -0600

    Solve set2 challenge15

---
+++ set2/challenge15/main.py
@@ -0,0 +1,29 @@
+from set2.challenge09.pkcs7_padding import *
+
+
+def main():
+    """
+    Already implemented `pkcs7_unpad()` in set2.challenge09
+    Display that it solves the challenge
+    """
+    b = b"ICE ICE BABY\x04\x04\x04\x04"
+    print("pkcs7_unpad(%s):" % b)
+    print(pkcs7_unpad(b))
+
+    b = b"ICE ICE BABY\x05\x05\x05\x05"
+    print("pkcs7_unpad(%s):" % b)
+    try:
+        pkcs7_unpad(b)
+    except (InvalidPaddingException):
+        print("Exception raised")
+
+    b = b"ICE ICE BABY\x01\x02\x03\x04"
+    print("pkcs7_unpad(%s):" % b)
+    try:
+        pkcs7_unpad(b)
+    except (InvalidPaddingException):
+        print("Exception raised")
+
+
+if __name__ == "__main__":
+    main()
