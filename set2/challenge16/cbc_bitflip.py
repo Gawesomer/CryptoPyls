a05092f324dc44f10cc559de8fb92d223e35a0f6
Jean-Baptiste Jouband <jj.jouband@gmail.com>
2021-01-23 00:03:10 -0600

    Solve set2 challenge16

--- set2/challenge16/cbc_bitflip.py
+++ set2/challenge16/cbc_bitflip.py
@@ -57,3 +57,45 @@ def is_admin(encrypted: bytes, blksize: int = 16) -> bool:
     decrypted = pkcs7_unpad(padded)
 
     return b";admin=true;" in decrypted
+
+
+def main():
+    """
+    insert ";admin=true;" string inside encrypted message using only
+    `encryption_oracle()`
+    """
+    encryption_oracle = gen_encryption_oracle()
+
+    # `encryption_oracle()` prepends exactly two blocks
+    # add two blocks of our own, the first block will be used to mutate the
+    # second one to our desired string
+    plain2 = bytes(16)
+    plain3 = bytes(16)
+    hack_enc = encryption_oracle(plain2+plain3)
+    hack_enc_blocks = [b for b in blocks(hack_enc, 16)]
+
+    # knowing the blocks that are getting encrypted (we just picked them above)
+    # we can figure out the AES decryption of the second block we added
+    decrypted_block3 = xor(hack_enc_blocks[2], plain3)
+
+    # the AES decryption of our block will get XORed with the previous block
+    # (i.e. the first block we added)
+    # simply XOR decrypted and desired to figure out what first block we need
+    # to use to mutate the second block as desired
+    desired_block = b"\x00\x00\x00\x00;admin=true;"
+    encrypted2 = xor(decrypted_block3, desired_block)
+
+    # remix the encrypted message by replacing the encrypted block of the first
+    # block we added with the block that will give us the required mutation
+    # all other blocks can remain the same as changing one block will only
+    # mutate the decryption of the next one and won't change the ones further
+    # down (i.e. not risk of ruining the padding down the line)
+    encrypted = hack_enc_blocks[0] + hack_enc_blocks[1] + encrypted2
+    for i in range(3, len(hack_enc_blocks)):
+        encrypted += hack_enc_blocks[i]
+
+    print(is_admin(encrypted))
+
+
+if __name__ == "__main__":
+    main()
